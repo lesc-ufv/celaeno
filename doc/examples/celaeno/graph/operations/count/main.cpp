7f9f20bcb226fc5e98ec4869581ea9b938b6ffb3
Ruan E. Formigoni <ruanformigoni@gmail.com>
2020-08-15 17:03:06 -0300

    Included examples #90 for #50 and #51

---
+++ doc/examples/celaeno/graph/operations/count/main.cpp
@@ -0,0 +1,67 @@
+// vim: set expandtab fdm=marker ts=2 sw=2 tw=100 et :
+//
+// @author      : Ruan E. Formigoni (ruanformigoni@gmail.com)
+// @file        : main.cpp
+// @created     : terça jul 28, 2020 07:49:14 -03
+//
+// BSD 2-Clause License
+
+// Copyright (c) 2020, Ruan Evangelista Formigoni
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include <iostream>
+#include <cstdlib>
+#include <fmt/ranges.h>
+#include <taygete/graph/graph.hpp>
+#include <celaeno/aliases.hpp>
+#include <celaeno/graph/operations/count/crossings/pipeline.hpp>
+
+
+// namespaces {{{
+namespace graph = taygete::graph;
+namespace count = celaeno::graph::operations::count::crossings::pipeline;
+// }}}
+
+int main()
+{
+  // Graph {{{
+  graph::Graph<i64> g
+  {
+    {1,5},{2,4},{3,4},{6,9},
+    {4,7},{5,7},{5,8},{5,9},
+    {7,12},{8,10},{8,11},
+  };
+  // }}}
+
+  // Obtain predecessor and successor nodes {{{
+  auto p = [&g](auto&& v){ return g.predecessors(v); };
+  auto s = [&g](auto&& v){ return g.successors(v); };
+  // }}}
+
+  // Print to stdout {{{
+  fmt::print("{}\n",count::run(1,p,s));
+  // }}}
+
+  return EXIT_SUCCESS;
+} // main
