a54044b850db18cc43cd64e5792d3b0dd0647ad5
Ruan E. Formigoni <ruanformigoni@gmail.com>
2020-08-19 11:52:57 -0300

    Included example #90 for #89

---
+++ doc/examples/celaeno/graph/views/proximity/main.cpp
@@ -0,0 +1,83 @@
+// vim: set expandtab fdm=marker ts=2 sw=2 tw=80 et :
+//
+// @author      : Ruan E. Formigoni (ruanformigoni@gmail.com)
+// @file        : main
+// @created     : quarta ago 19, 2020 11:44:31 -03
+//
+// BSD 2-Clause License
+
+// Copyright (c) 2020, Ruan Evangelista Formigoni
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include <iostream>
+#include <cstdlib>
+#include <fplus/fplus.hpp>
+#include <fmt/ranges.h>
+#include <taygete/graph/graph.hpp>
+#include <celaeno/aliases.hpp>
+#include <celaeno/graph/views/proximity.hpp>
+
+
+// namespaces {{{
+namespace fp = fplus;
+namespace graph = taygete::graph;
+namespace proximity = celaeno::graph::views::proximity;
+// }}}
+
+int main()
+{
+  // Graph {{{
+  graph::Graph<i64> g
+  {
+    {1,5},{2,4},{3,4},{6,9},
+    {4,7},{5,7},{5,8},{5,9},
+    {7,12},{8,10},{8,11},
+  };
+  // }}}
+
+  // Required behaviors {{{
+  auto p = [&g](auto&& u){ return g.predecessors(u); };
+  auto s = [&g](auto&& u){ return g.successors(u); };
+  // }}}
+
+  // Run algorithm {{{
+  auto proximity_view {proximity::run(1,p,s).first};
+  // }}}
+
+  // Print topological view {{{
+  i64 curr_layer{0};
+  for (auto&& layer : proximity_view)
+  {
+    fmt::print("{} ", layer.second);
+    if( layer.first != curr_layer )
+    {
+      curr_layer = layer.first;
+      fmt::print("\n");
+    }
+  } // for layer : proximity_view
+  fmt::print("\n");
+  // }}}
+
+  return EXIT_SUCCESS;
+} // main
