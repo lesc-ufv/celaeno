6d5f80b210791cd06f701af745b61b478d0fcae7
Ruan E. Formigoni <ruanformigoni@gmail.com>
2020-11-15 11:03:00 -0300

    Included single-header include #109

---
+++ include/celaeno/celaeno.hpp
@@ -0,0 +1,29 @@
+// vim: set expandtab fdm=marker ts=2 sw=2 tw=80 et :
+//
+// @author      : Ruan E. Formigoni (ruanformigoni@gmail.com)
+// @file        : celaeno
+// @created     : domingo nov 15, 2020 10:53:45 -03
+//
+
+#pragma once
+
+// Operations {{{
+#include <celaeno/graph/operations/balance/paths.hpp>
+#include <celaeno/graph/operations/count/crossings.hpp>
+#include <celaeno/graph/operations/minimize/crossings.hpp>
+// }}}
+
+// Representations {{{
+#include <celaeno/graph/representations/incidence.hpp>
+// }}}
+
+// Search {{{
+#include <celaeno/graph/search/bfs.hpp>
+#include <celaeno/graph/search/dfs.hpp>
+#include <celaeno/graph/search/kahn.hpp>
+// }}}
+
+// Drawing {{{
+#include <celaeno/graph/draw/svg.hpp>
+// }}}
+
