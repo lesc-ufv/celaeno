8a4668fa841fa91256f1facb8be53ee6e7e58b76
Ruan E. Formigoni <ruanformigoni@gmail.com>
2021-02-07 13:45:07 -0300

    Fixed docker build error

--- conanfile.py
+++ conanfile.py
@@ -43,8 +43,8 @@ class CelaenoConan(ConanFile):
 
   def requirements(self):
     self.requires('doctest/2.4.1')
-    self.requires('spdlog/1.8.2')
-    self.requires('fmt/7.1.3')
+    self.requires('spdlog/1.4.2@bincrafters/stable')
+    self.requires('fmt/6.0.0@bincrafters/stable')
     self.requires('functionalplus/v0.2.13-p0@dobiasd/stable')
     self.requires('range-v3/0.11.0@ericniebler/stable')
 
