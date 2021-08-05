a68d1799ccc6ac6f602af3ac4cb18cf5caad7e49
Ruan Formigoni <ruanformigoni@gmail.com>
2021-08-05 23:32:12 +0000

    Included chebyshev heuristic

---
+++ include/celaeno/heuristics/chebyshev.hpp
@@ -0,0 +1,59 @@
+// vim: set expandtab fdm=marker ts=2 sw=2 tw=80 et :
+//
+// @author      : Ruan E. Formigoni (ruanformigoni@gmail.com)
+// @file        : chebyshev
+// @created     : Thursday Aug 05, 2021 23:28:52 UTC
+//
+// BSD 2-Clause License
+
+// Copyright (c) 2020, Ruan Evangelista Formigoni
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#pragma once
+
+#include <cmath>
+#include <utility>
+
+#include <celaeno/aliases.hpp>
+#include <celaeno/concepts.hpp>
+
+namespace celaeno::heuristics::chebyshev
+{
+
+// Aliases {{{
+using namespace celaeno::aliases;
+using namespace celaeno::concepts;
+
+template<typename T>
+using Coord = std::pair<T,T>;
+// }}}
+
+// fn: run {{{
+template<SignedIntegral T>
+constexpr auto run(Coord<T> p1, Coord<T> p2)
+{
+  return std::max(std::abs(p1.first - p2.first),std::abs(p1.second - p2.second));
+} // function: run }}}
+
+} // namespace celaeno::heuristics::chebyshev
