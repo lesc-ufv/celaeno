8bbd880a0e5a023b2d916ca7bd3ead1c90b55680
Ruan E. Formigoni <ruanformigoni@gmail.com>
2024-06-03 15:21:26 -0300

    Enable build on setup_compile_commands

--- applet/compile_commands/setup_compile_commands.sh
+++ applet/compile_commands/setup_compile_commands.sh
@@ -8,7 +8,7 @@ DIR_PROJECT="$(dirname "$(dirname "$DIR_SCRIPT")")"
 
 cd "$DIR_PROJECT"
 
-# docker build . -t celaeno -f docker/Dockerfile.applets --build-arg="CELAENO_DIR=$(pwd)"
+docker build . -t celaeno -f docker/Dockerfile.applets --build-arg="CELAENO_DIR=$(pwd)"
 
 docker run -it --rm \
   -e DIR_PROJECT="$DIR_PROJECT" \
