ce8a9d2cf0380aa36e2709bcbce8da4cdef7690e
Ruan E. Formigoni <ruanformigoni@gmail.com>
2024-06-03 16:37:57 -0300

    Copy static binary to host

--- applet/compile/compile.sh
+++ applet/compile/compile.sh
@@ -8,8 +8,13 @@ DIR_PROJECT="$(dirname "$(dirname "$DIR_SCRIPT")")"
 
 cd "$DIR_PROJECT"
 
+# Compile the project
 docker build . -t celaeno -f docker/Dockerfile.applets --build-arg="CELAENO_DIR=$(pwd)"
 
+# Copy the built binary
+docker run -it --rm -v "$(pwd)":/host celaeno cp ./build/Release/applet/mapper /host/applet/mapper
+
+# Setup compile_commands and conan libraries
 docker run -it --rm \
   -e DIR_PROJECT="$DIR_PROJECT" \
   -v "$(pwd)":/host \
