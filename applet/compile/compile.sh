58940f74d29112c444f46170368d31f7e34c5e1c
Ruan E. Formigoni <ruanformigoni@gmail.com>
2024-06-03 16:31:17 -0300

    Updated compile.sh

--- applet/compile/compile.sh
+++ applet/compile/compile.sh
@@ -13,4 +13,4 @@ docker build . -t celaeno -f docker/Dockerfile.applets --build-arg="CELAENO_DIR=
 docker run -it --rm \
   -e DIR_PROJECT="$DIR_PROJECT" \
   -v "$(pwd)":/host \
-  celaeno /host/applet/compile_commands/impl_compile.sh
+  celaeno /host/applet/compile/impl_compile.sh
